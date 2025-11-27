21563c4c291c10bba106abe3ea2dae224d2c3aaa
Ramón Espinoza <ramon.espinoza.s@mail.pucv.cl>
2025-11-27 20:54:35 -0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-from experiment import run_experiment
-from plots import plot_results
-
-if __name__ == "__main__":
-    results = run_experiment(reps=30)
-    print(results)
-    plot_results(results)
